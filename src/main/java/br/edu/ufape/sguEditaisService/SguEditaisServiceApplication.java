6467f3e70225a55f4645f25f8aa210871db34cfa
Douglas Filipe <batista.douglasfs@gmail.com>
2025-05-15 10:58:49 -0300

    commit inicial

---
+++ src/main/java/br/edu/ufape/sguEditaisService/SguEditaisServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.sguEditaisService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SguEditaisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SguEditaisServiceApplication.class, args);
+	}
+
+}

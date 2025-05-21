d88b82b50216af0dd65df8671f1e1a9576005c15
GENILDOBURGOS <gnldburgos@gmail.com>
2025-05-21 15:38:49 -0300

    feat: cria enums para status, status etapa e tipo campo

---
+++ src/main/java/br/edu/ufape/sguEditaisService/models/enums/status.java
@@ -0,0 +1,11 @@
+package br.edu.ufape.sguEditaisService.models.enums;
+
+public enum status {
+    ATIVO,
+    INATIVO,
+    CANCELADO,
+    EM_INSCRICOES,
+    HOMOLOGADO,
+    CONCLUIDO,
+    SUSPENSO
+}
---
+++ src/main/java/br/edu/ufape/sguEditaisService/models/enums/statusEtapa.java
@@ -0,0 +1,9 @@
+package br.edu.ufape.sguEditaisService.models.enums;
+
+public enum statusEtapa {
+    PEDENTE,
+    EM_ANDAMENTO,
+    CONCLUIDA,
+    ATRASADA,
+    CANCELADA
+}

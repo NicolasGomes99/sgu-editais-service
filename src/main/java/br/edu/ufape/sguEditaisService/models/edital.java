a3c64a42a91de60b49add0f305d9844c1e086ea7
GENILDOBURGOS <gnldburgos@gmail.com>
2025-05-22 15:15:15 -0300

    fix: adicionar atributo idUnidadeAdministrativa à classe edital

--- src/main/java/br/edu/ufape/sguEditaisService/models/edital.java
+++ src/main/java/br/edu/ufape/sguEditaisService/models/edital.java
@@ -29,6 +29,8 @@ public class edital {
     @OneToOne
     private tipoEdital tipoEdital;
 
+    private Long idUnidadeAdministrativa;
+
     @OneToMany(mappedBy = "edital", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<etapa> etapas;
 

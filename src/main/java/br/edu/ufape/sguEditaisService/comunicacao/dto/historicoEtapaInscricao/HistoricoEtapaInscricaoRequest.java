fb21d3a4f81780e295bc6b0474d4050728725a51
Vitorass0 <vitor.ass@hotmail.com>
2025-06-11 14:27:35 -0300

    Fix: Corrigindo atributo

--- src/main/java/br/edu/ufape/sguEditaisService/comunicacao/dto/historicoEtapaInscricao/HistoricoEtapaInscricaoRequest.java
+++ src/main/java/br/edu/ufape/sguEditaisService/comunicacao/dto/historicoEtapaInscricao/HistoricoEtapaInscricaoRequest.java
@@ -1,11 +1,12 @@
 package br.edu.ufape.sguEditaisService.comunicacao.dto.historicoEtapaInscricao;
 
+import br.edu.ufape.sguEditaisService.comunicacao.dto.etapa.EtapaRequest;
+import br.edu.ufape.sguEditaisService.comunicacao.dto.inscricao.InscricaoRequest;
 import br.edu.ufape.sguEditaisService.models.HistoricoEtapaInscricao;
 import br.edu.ufape.sguEditaisService.models.enums.status;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import org.modelmapper.ModelMapper;
-
 import java.util.Date;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
@@ -15,7 +16,9 @@ public class HistoricoEtapaInscricaoRequest {
     @NotNull(message = "Status é obrigatório")
     private status status;
     @NotBlank(message = "Descrição é obrigatória")
-    private String observação;
+    private String observacao;
+    private InscricaoRequest inscricao;
+    private EtapaRequest etapa;
 
     public HistoricoEtapaInscricao convertToEntity(HistoricoEtapaInscricaoRequest request, ModelMapper modelMapper) {
         return modelMapper.map(request, HistoricoEtapaInscricao.class);

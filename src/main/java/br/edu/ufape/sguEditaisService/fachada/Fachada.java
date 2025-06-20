7e126704ef365f0ccdf7b3489249da099756f0de
GENILDOBURGOS <gnldburgos@gmail.com>
2025-06-19 22:37:44 -0300

    feat: inicio fachada

---
+++ src/main/java/br/edu/ufape/sguEditaisService/fachada/Fachada.java
@@ -0,0 +1,10 @@
+package br.edu.ufape.sguEditaisService.fachada;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Fachada {
+
+}

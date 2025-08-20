4231d456ca8178485b6e15f0d9062a87b8c516cf
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2025-08-20 11:47:52 -0300

    feat: ajustando campo personalizado

--- src/main/java/br/edu/ufape/sguEditaisService/servicos/interfaces/CampoPersonalizadoService.java
+++ src/main/java/br/edu/ufape/sguEditaisService/servicos/interfaces/CampoPersonalizadoService.java
@@ -2,6 +2,8 @@ package br.edu.ufape.sguEditaisService.servicos.interfaces;
 
 import br.edu.ufape.sguEditaisService.exceptions.notFound.CampoPersonalizadoNotFoundException;
 import br.edu.ufape.sguEditaisService.models.CampoPersonalizado;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,9 +12,17 @@ public interface CampoPersonalizadoService {
 
     CampoPersonalizado buscarPorIdCampoPersonalizado(Long id) throws CampoPersonalizadoNotFoundException;
 
-    List<CampoPersonalizado> listarCampoPersonalizado();
+    Page<CampoPersonalizado> listarCampoPersonalizado(Pageable pageable);
 
     CampoPersonalizado editarCampoPersonalizado(Long id, CampoPersonalizado entity) throws CampoPersonalizadoNotFoundException;
 
     void deletarCampoPersonalizado(Long id) throws CampoPersonalizadoNotFoundException;
+
+    List<CampoPersonalizado> listarCamposPorEdital(Long editalId);
+
+    List<CampoPersonalizado> listarCamposPorTipoEdital(Long tipoEditalModeloId);
+
+    List<CampoPersonalizado> listarCamposPorEtapa(Long etapaId);
+
+    CampoPersonalizado alternarObrigatoriedade(Long campoPersonalizadoId);
 }

501008a5d08311fa899655017e2fa7a181eb5c97
Vitorass0 <vitor.ass@hotmail.com>
2025-09-17 20:50:28 -0300

    Feat: Adicionando lógica para a unidade administrativa ser preenchida automaticamente.

--- src/main/java/br/edu/ufape/sguEditaisService/servicos/interfaces/AuthServiceHandler.java
+++ src/main/java/br/edu/ufape/sguEditaisService/servicos/interfaces/AuthServiceHandler.java
@@ -2,6 +2,8 @@ package br.edu.ufape.sguEditaisService.servicos.interfaces;
 
 import br.edu.ufape.sguEditaisService.comunicacao.dto.unidadeAdministrativa.UnidadeAdministrativaResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.data.domain.Page;
+import java.util.UUID;
 
 public interface AuthServiceHandler {
 
@@ -10,4 +12,16 @@ public interface AuthServiceHandler {
 
     @SuppressWarnings("unused")
     UnidadeAdministrativaResponse fallbackBuscarUnidadeAdministrativa(Long id, Throwable t);
+
+    @CircuitBreaker(name = "authServiceClient", fallbackMethod = "fallbackListarUnidadesDoFuncionarioPorId")
+    Page<UnidadeAdministrativaResponse> listarUnidadesDoFuncionarioPorId(UUID usuarioId);
+
+    @SuppressWarnings("unused")
+    Page<UnidadeAdministrativaResponse> fallbackListarUnidadesDoFuncionarioPorId(UUID usuarioId, Throwable t);
+
+    @CircuitBreaker(name = "authServiceClient", fallbackMethod = "fallbackListarUnidadesDoGestorPorId")
+    Page<UnidadeAdministrativaResponse> listarUnidadesDoGestorPorId(UUID usuarioId);
+
+    @SuppressWarnings("unused")
+    Page<UnidadeAdministrativaResponse> fallbackListarUnidadesDoGestorPorId(UUID usuarioId, Throwable t);
 }
